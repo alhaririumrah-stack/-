274667e9ed2a7f8ce91f80044aec621ae4184ae1
alhaririumrah-stack <alhariri.umrah@gmail.com>
2026-06-23 22:26:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  API_URL: 'PUT_YOUR_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzmmNnSZrESJLeB5hpjKu2n4Ek_22mm33zTGY2Ke3eO3Z41IhudTK0ZNpqdo9OMaHgxGQ/exec',
   APP_NAME: 'نظام تشغيل العمرة ERP'
 };
